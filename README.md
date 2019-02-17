17a455101ed84c90eef10b36ad39d92f0ee8f1a5
E. Dyavolova <e.dyavolova@baldar.eu>
2019-02-17 22:35:55 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Dashboard Bulgaria Map for Open Cart 3
+======
+
+SVG map from: https://mapsvg.com/maps/bulgaria/
+Post codes from: http://www.crc.bg/files/_bg/oo12.htm
+Newer files for the jQuery Vector Map Library from: https://github.com/10bestdesign/jqvmap/
---
+++ upload/admin/language/bg/extension/dashboard/map.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title'] = 'Карта на България';
+// Text
+$_['text_extension'] = 'Допълнения';
+$_['text_success'] = 'Успех: Вие успешно променихте настройките!';
+$_['text_edit'] = 'Редакция на настройките';
+$_['text_order'] = 'Поръчки';
+$_['text_sale'] = 'Продажби';
+// Entry
+$_['entry_status'] = 'Статус';
+$_['entry_sort_order'] = 'Подреждане';
+$_['entry_width'] = 'Ширина';
+
+// Error
+$_['error_permission'] = 'Вие нямате права да променяте настройките!';
+?>
